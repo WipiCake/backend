e8cacaf58b06cde53c8f5d4d26eedc1479b68e7f
jaemin4 <eheh25877@gmail.com>
2025-04-08 19:50:56 +0900

    FEAT : Utils.toJson

--- src/main/java/com/wipi/support/util/Utils.java
+++ src/main/java/com/wipi/support/util/Utils.java
@@ -1,19 +1,29 @@
 package com.wipi.support.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
+@Component
 public class Utils {
+
+    private final ObjectMapper injectedMapper;
     private static ObjectMapper objectMapper;
+
+    @PostConstruct
+    public void init() {
+        objectMapper = this.injectedMapper;
+    }
 
     public static String toJson(Object obj) {
         try {
             return objectMapper.writeValueAsString(obj);
         } catch (Exception e) {
-            log.error("JSON 변환 오류: {}", e.getMessage());
+            log.error("JSON 변환 오류: {}", e.getMessage(), e);
             return null;
         }
     }
