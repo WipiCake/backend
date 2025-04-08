f91f8920eabec1722860d78e6bb240a3c305b0e5
jaemin4 <eheh25877@gmail.com>
2025-04-09 00:17:06 +0900

    SET : init

--- src/main/java/com/wipi/exception/AuthRuntimeException.java
+++
@@ -1,8 +0,0 @@
-package com.wipi.exception;
-
-public class AuthRuntimeException extends CustomRuntimeExeption{
-    public AuthRuntimeException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/wipi/exception/BankRuntimeException.java
+++
@@ -1,7 +0,0 @@
-package com.wipi.exception;
-
-public class BankRuntimeException extends CustomRuntimeExeption {
-    public BankRuntimeException(String message) {
-        super(message);
-    }
-}
