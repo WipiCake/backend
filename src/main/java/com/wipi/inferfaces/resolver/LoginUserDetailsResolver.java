e352e76d545b2a9df09f7e718261764c032afdb9
jaemin4 <eheh25877@gmail.com>
2025-05-09 16:58:36 +0900

    fix : login resolver 인증되지 않은 사용자 접근시 예외 발생

--- src/main/java/com/wipi/inferfaces/resolver/LoginUserDetailsResolver.java
+++ src/main/java/com/wipi/inferfaces/resolver/LoginUserDetailsResolver.java
@@ -3,6 +3,8 @@ package com.wipi.inferfaces.resolver;
 import com.wipi.domain.user.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -25,14 +27,14 @@ public class LoginUserDetailsResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
-            return null;
+            throw new AccessDeniedException("로그인이 필요한 요청입니다.");
         }
 
         if (authentication.getPrincipal() instanceof CustomUserDetails) {
             return ((CustomUserDetails) authentication.getPrincipal()).getUser();
         }
 
-        return null;
+        throw new AccessDeniedException("사용자 정보가 유효하지 않습니다.");
     }
 
 }
